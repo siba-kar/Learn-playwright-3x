a61c0cbda912d703b861ccc11d6a22b01c0bd9e3
siba-kar <sskar1996@gmail.com>
2026-07-19 12:46:39 +0530

    Add more increment/decrement operator examples in 34_Incre_Part2.js

--- LearningPlaywright3x/05_chapter_Operator/34_Incre_Part2.js
+++ LearningPlaywright3x/05_chapter_Operator/34_Incre_Part2.js
@@ -11,7 +11,28 @@
 // console.log(++a + ++a);
 // console.log(a);
 
-let a = 34;
-let result = a++;
-console.log(result);
-console.log(a);
+// let a = 34;
+// let result = a++;
+// console.log(result);
+// console.log(a);
+
+// let a = 100;
+
+// console.log(a++ + ++a +a++ + ++a);
+
+// console.log(a);
+
+//  let a = 37;
+
+// console.log(--a + a--);
+
+// console.log(a);
+
+// let a = 5; 
+// let b = a-- - --a; 
+// console.log(b, a);
+
+let i = 1; 
+
+let r = i++ > 1 ? i++ : ++i; 
+console.log(r, i); 
